2be0eb3455a7a8289609f353f6645e8c17a6f549
aleezatahir <taleeza97@gmail.com>
2026-07-26 13:06:27 +0500

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,11 @@
+info ={
+    "name": "aleeza tahir",
+    "subjects" : ["c", "java","Python"],
+    "topic" : ("dict", "set"),
+    "Age" : "35",
+    " is_adult": "True",
+     "marks": 94.4
+}
+
+print(info)
+
