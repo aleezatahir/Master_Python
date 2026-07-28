ee3d60499bdca59f948e84f0b9a4a65b769f8382
aleezatahir <taleeza97@gmail.com>
2026-07-28 13:01:43 +0500

    Add files via upload

---
+++ rang.py
@@ -0,0 +1,14 @@
+#seq = range(10)
+#for i in seq:
+   # print(i)
+
+#for i in range(10):# range stop
+   # print(i)
+
+#for i in range(2,10):# range start and stop
+    # print(i)
+for i in range(1,10,2):# range start, stop and step
+    print(i)
+
+        
+
