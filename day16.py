b2a241d05d126b9d18c669fc372ba4e695dc35af
aleezatahir <taleeza97@gmail.com>
2026-08-08 21:49:40 +0500

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,10 @@
+speed = int(input("Enter your car speed: "))
+
+if speed < 40:
+    print("You are driving slowly.")
+elif speed <= 80:
+    print("Perfect speed! Drive safely.")
+else:
+    print("You are driving too fast!")
+
+print("Have a safe journey!")
